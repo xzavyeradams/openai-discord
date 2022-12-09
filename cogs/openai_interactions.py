b21dea5ded4f31fb610f9642a5ec68bb5495f3af
Xzavyer Adams <30382793+xzavyeradams@users.noreply.github.com>
2022-12-09 07:53:04 -0500

    add describe decorators

--- cogs/openai_interactions.py
+++ cogs/openai_interactions.py
@@ -3,36 +3,48 @@ import discord
 import aiohttp
 from discord import app_commands
 from discord.ext import commands
-
-# I apologize if any of this is incorrect, I'm on a chromebook at the time of this commit
-# and don't have easy access to documentation or testing.
+from typing import Optional
 
 class GPT3(commands.GroupCog, name="gpt3", group_description="Generates text based content based on a prompt using AI."):
     def __init__(self, bot: commands.Bot, auth: str, tkn: int, org: str):
         self.bot = bot
         self.client = aiohttp.ClientSession()
         self.headers = {"Authorization": "Bearer " + auth}
+        if org != "default":
+            self.headers.update({"OpenAI-Organization": org})
         self.max = tkn
         self.org = org
-        
+
         super().__init__()
 
     @app_commands.command(name="davinci", description="GPT-3 text-davinci-003 | Generates text based on prompt.")
-    async def davinci3(self):
+    @app_commands.describe(prompt="Prompt",
+                            maxLen="Max # of tokens to use for this request.",
+                            temp="(Decimal between 0-1) Controls randomness.")
+    async def davinci3(self, prompt: str, maxLen: Optional[int], temp: Optional[float]):
         """ /davinci3 """
         return
     
     @app_commands.command(name="curie", description="GPT-3 text-curie-001 | Less capable, but faster & cheaper.")
+    @app_commands.describe(prompt="Prompt",
+                            maxLen="Max # of tokens to use for this request.",
+                            temp="(Decimal between 0-1) Controls randomness.")
     async def curie(self):
         """ /curie """
         return
 
     @app_commands.command(name="babbage", description="GPT-3 text-babbage-001 | Straight-forward, faster & cheaper.")    
+    @app_commands.describe(prompt="Prompt",
+                            maxLen="Max # of tokens to use for this request.",
+                            temp="(Decimal between 0-1) Controls randomness.")
     async def babbage(self):
         """ /babbage """
         return
     
     @app_commands.command(name="ada", description="GPT-3 text-ada-001 | Very simple tasks, usually fastest, lowest cost.")
+    @app_commands.describe(prompt="Prompt",
+                            maxLen="Max # of tokens to use for this request.",
+                            temp="(Decimal between 0-1) Controls randomness.")
     async def ada(self):
         """ /ada """
         return
